34908df9ecde1c5c007a52575d991c69f0ab34b3
Anant Verma <anantverma2001@gmail.com>
2021-03-01 17:37:14 +0530

    homescreen checkpoint

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -58,6 +58,7 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin<Home> {
                 Navigator.pushNamed(context, ProfileScreen.routeName);
               },
             ),
+
           ],
         ),
         drawer: AppDrawer(),
@@ -90,6 +91,7 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin<Home> {
         load = false;
       });
     });
+
   }
 }
 
