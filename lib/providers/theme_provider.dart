ec0fc99faaa228046d1a7d3693b8d24c5b10c4f2
khyatisaini <sainikhyati2001@gmail.com>
2021-03-02 09:28:43 +0530

    fixing initial theme issue

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:paradox/models/brightness_options.dart';
-import 'package:paradox/utilities/dark_theme_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  DarkThemePreferences darkThemePreferences = new DarkThemePreferences();
-  BrightnessOption brightnessOption = BrightnessOption.light;
+  BrightnessOption brightnessOption;
+  static const THEME_MODE = "dark-mode";
 
+  ThemeProvider() {
+    brightnessOption = BrightnessOption.light;
+    // retrieving current app theme
+    getThemePreference();
+  }
+
+  // function to set theme
   void setBrightnessOption(BrightnessOption option) {
     brightnessOption = option;
-    bool value = (brightnessOption == BrightnessOption.dark) ? true : false;
-    darkThemePreferences.setDarkTheme(value);
+    // updating shared preferences for theme
+    setDarkTheme();
     print('Theme mode: $brightnessOption');
+    notifyListeners();
+  }
+
+  // setting value in shared preferences cased on the selected theme
+  setDarkTheme() async {
+    bool value = brightnessOption == BrightnessOption.light ? false : true;
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.setBool(THEME_MODE, value);
+  }
+
+  // getting theme preference from shared preferences
+  Future getThemePreference() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    brightnessOption = preferences.getBool(THEME_MODE) ?? false ? BrightnessOption.dark : BrightnessOption.light;
     notifyListeners();
   }
 
