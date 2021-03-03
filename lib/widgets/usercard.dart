01503783e3582443309f70ffcdc83da7da3084b1
khyatisaini <sainikhyati2001@gmail.com>
2021-03-03 10:23:32 +0530

    dark mode leaderboard ui

--- lib/widgets/usercard.dart
+++ lib/widgets/usercard.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:paradox/models/brightness_options.dart';
 import 'package:paradox/models/leaderBoardUser.dart';
+import 'package:paradox/providers/theme_provider.dart';
 import 'package:paradox/screens/photo_view.dart';
+import 'package:provider/provider.dart';
 
 class UserCardWidget extends StatelessWidget {
   final LeaderBoardUser user;
@@ -10,44 +13,54 @@ class UserCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final brightness = Provider.of<ThemeProvider>(context, listen: true).brightnessOption;
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 15),
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
       child: Row(
         children: [
           Expanded(
               child: Center(
                   child: Text(
-            '$index',
-            style: TextStyle(
-                color: Colors.blue[700],
-                fontSize: 26,
-                fontWeight: FontWeight.w600),
-          ))),
+                    '$index',
+                    style: TextStyle(
+                      color: brightness == BrightnessOption.light ? Color(0xff0083B0) : Colors.grey,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400),
+              ),
+            ),
+          ),
           Expanded(
-              flex: 4,
+              flex: 5,
               child: Container(
+                margin: EdgeInsets.only(right: 20),
                 decoration: BoxDecoration(
                     gradient: LinearGradient(
                       colors: [
-                        Color(0xff0083B0),
-                        Color(0xff00B4DB),
+                        brightness == BrightnessOption.light ? Color(0xff0083B0) : Colors.white.withOpacity(0.9),
+                        brightness == BrightnessOption.light ? Color(0xff00B4DB) : Colors.grey[400],
                         // Color(0xff1A2980),
                       ],
                     ),
                     boxShadow: [
+                      brightness == BrightnessOption.light ?
                       BoxShadow(
                         color: Color(0xff0083B0),
                         blurRadius: 10,
                         offset: Offset(0, 4),
-                      ),
+                      )
+                      : BoxShadow(),
                     ],
                     borderRadius: BorderRadius.circular(35)),
                 child: Row(
                   children: [
                     GestureDetector(
                       child: CircleAvatar(
-                        backgroundImage: new NetworkImage(user.image),
                         radius: 35,
+                        backgroundColor: Colors.transparent,
+                        child: CircleAvatar(
+                          backgroundImage: new NetworkImage(user.image),
+                          radius: 33,
+                        ),
                       ),
                       onTap: () {
                         Navigator.of(context).push(MaterialPageRoute(
@@ -57,22 +70,24 @@ class UserCardWidget extends StatelessWidget {
                     ),
                     Expanded(
                         child: Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: Text(
-                        user.name,
-                        style: TextStyle(color: Colors.white),
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: Text(
+                          user.name,
+                          style: TextStyle(color: brightness == BrightnessOption.light ? Colors.white : Colors.grey[800]),
+                        ),
                       ),
-                    )),
+                    ),
                     Padding(
                       padding: const EdgeInsets.only(right: 30),
                       child: Text(
                         user.score.toString(),
-                        style: TextStyle(color: Colors.white),
+                        style: TextStyle(color: brightness == BrightnessOption.light ? Colors.white : Colors.grey[800]),
                       ),
                     )
                   ],
                 ),
-              ))
+              ),
+          ),
         ],
       ),
     );
